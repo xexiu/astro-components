1bd8dcda2328125d3940ef02456c990536f23bdf
Xexiu <sergiu.mironescu@gmail.com>
2023-10-21 10:18:11 +0200

    added lint for astro

--- astro-modal/__tests__/astro-modal.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const astroModal = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(astroModal(), 'Hello from astroModal');
-console.info('astroModal tests passed');
---
+++ astro-modal/src/index.ts
@@ -0,0 +1,5 @@
+import Modal from './Modal.astro'
+
+export {
+    Modal as default
+}
